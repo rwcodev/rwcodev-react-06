8fdfa65cdfa415056988d56b3477168e021e2a5d
R. Walter <133987890+rwcodev@users.noreply.github.com>
2024-05-30 11:24:23 -0600

    Delete src directory

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import trainer from './slices/trainer.slice'
-
-export default configureStore ({
-    reducer: {
-        trainer        
-    }
-})
